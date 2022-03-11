f7daaebbdba7bd544a91a12673220a86fe51e05d
mohammad shirvan <s.shirvan@gmail.com>
2022-03-11 18:49:16 -0500

    first commit

---
+++ src/main/java/com/cantel/springrestawsecspipelinecicd/SpringRestAwsEcsPipelineCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.cantel.springrestawsecspipelinecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestAwsEcsPipelineCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestAwsEcsPipelineCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cantel/springrestawsecspipelinecicd/SpringRestAwsEcsPipelineCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cantel.springrestawsecspipelinecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestAwsEcsPipelineCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
